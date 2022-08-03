06b0587a4b68099c4932012283cb0e1835edf293
Malik Banuaji <ridwanmalik88@gmail.com>
2022-08-03 23:10:32 +0700

    feat: add pull to refresh

--- src/screens/contact-list/contact-list.js
+++ src/screens/contact-list/contact-list.js
@@ -1,162 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
-// import BottomSheet from '@gorhom/bottom-sheet';
-import {AlertModal} from '../../components';
-import colors from '../../theme/colors';
-import ContactItem from './contact-item';
-import Header from './header';
-import ModalContent from './modal-content';
 import {useDispatch, useSelector} from 'react-redux';
+import {AlertModal} from '../../components';
 import {
   fetchAllContact,
   selectAllContact,
 } from '../../features/contact/contactSlice';
-
-const LIST_CONTACT = {
-  data: [
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: 'b3abd640-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Luke',
-      lastName: 'Skywalker',
-      age: 20,
-      photo: 'N/A',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-    {
-      id: '93ad6070-c92b-11e8-b02f-cbfa15db428b',
-      firstName: 'Bilbo',
-      lastName: 'Baggins',
-      age: 111,
-      photo:
-        'http://vignette1.wikia.nocookie.net/lotr/images/6/68/Bilbo_baggins.jpg/revision/latest?cb=20130202022550',
-    },
-  ],
-};
-
-const modalContentAction = [
-  {
-    key: 'view',
-    label: 'View',
-    onPress: () => {},
-  },
-  {
-    key: 'edit',
-    label: 'Edit',
-    onPress: () => {},
-  },
-  {
-    key: 'delete',
-    label: 'Delete',
-    onPress: () => {},
-  },
-];
+import colors from '../../theme/colors';
+import ContactItem from './contact-item';
+import ModalContent from './modal-content';
 
 const ContactList = ({navigation}) => {
-  const [selectedContact, setSelectedContact] = useState({
-    id: null,
-    index: null,
-  });
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
   const contactList = useSelector(selectAllContact);
 
+  const _handleOnRefresh = async () => {
+    setLoading(true);
+    await dispatch(fetchAllContact());
+    setLoading(false);
+  };
+
   useEffect(() => {
-    dispatch(fetchAllContact());
+    const fetchContact = async () => {
+      setLoading(true);
+      await dispatch(fetchAllContact());
+      setLoading(false);
+    };
+    fetchContact();
   }, [dispatch]);
 
   const _renderItem = ({item, index}) => {
@@ -168,34 +40,20 @@ const ContactList = ({navigation}) => {
         onPress={() =>
           navigation.navigate('ContactAdd', {type: 'view', index, id: item.id})
         }
-        onLongPress={() => {
-          setSelectedContact({id: item.id, index});
-        }}
       />
     );
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Header onPressAdd={() => navigation.navigate('ContactAdd')} /> */}
       <FlatList
+        refreshing={loading}
+        onRefresh={_handleOnRefresh}
         contentContainerStyle={styles.flatListContentContainer}
         data={contactList}
         keyExtractor={(item, index) => `${item.id} ${index}`}
         renderItem={_renderItem}
       />
-      <AlertModal
-        onBackButtonPress={() => setSelectedContact({id: null, index: null})}
-        onBackdropPress={() => setSelectedContact({id: null, index: null})}
-        isVisible={selectedContact.id !== null}>
-        <View style={styles.modalContentContainer}>
-          <ModalContent
-            // data={contactList[selectedContact.index]}
-            actions={modalContentAction}
-          />
-          <View />
-        </View>
-      </AlertModal>
     </SafeAreaView>
   );
 };
